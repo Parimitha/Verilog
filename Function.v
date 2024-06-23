ef1286694a88c5a631d708bddea21815ec969408
Parimitha <119649002+Parimitha@users.noreply.github.com>
2024-06-23 18:28:24 +0530

    Function.v

---
+++ Function.v
@@ -0,0 +1,15 @@
+module func;
+  function compare(input int a, b);
+    if(a>b)
+      $display("a is greater than b");
+    else if(a<b)
+      $display("a is less than b");
+    else 
+      $display("a is equal to b");
+  endfunction
+  initial begin
+    compare(10,10);
+    compare(5, 9);
+    compare(9, 5);
+  end
+endmodule
